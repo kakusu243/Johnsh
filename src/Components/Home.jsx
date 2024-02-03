f7ccd072e2b072e8c940870a76cee7e7fdc1c63a
kakusu243 <138121099+kakusu243@users.noreply.github.com>
2024-02-03 08:21:06 +0000

    photo de profile changée

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/johnphoto.jpg";
+import image from "../images/johnkakusu1.jpg";
 
 const imageAltText = "un homme noir qui souri";
 
