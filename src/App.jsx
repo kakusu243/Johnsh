ecd2b6db1d08c223bbb063f9eb969ed6b69cb186
kakusu243 <138121099+kakusu243@users.noreply.github.com>
2024-02-03 07:31:51 +0000

    changement des information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "John Kakusu",
+  title: "Desktop developper & Content Creator",
+  email: "johnshekinah243@gmail.com",
+  gitHub: "https://github.com/kakusu243",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/john-kakusu-1b6b7b272/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/kakusu243",
+  youTube: "https://www.youtube.com/channel/UCrYDbx-O3QR6_bKmzGpvFiw",
 };
 
 const primaryColor = "#4E567E";
