bffbb27c6344d5a7fdffef20716c3e887553651d
kakusu243 <138121099+kakusu243@users.noreply.github.com>
2024-02-04 13:21:02 +0000

    backgroudAbout changed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/image2.jpg";
+import image from "../images/backgroundAbout.jpg";
 
 const imageAltText = "Blue Strings background";
 
